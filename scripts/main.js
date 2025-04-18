4ecd7da1484c6a8d8a3e14b7bd4f2ef8ff848728
Adroaldo <hygino@msn.com>
2025-04-18 16:51:43 -0300

    Manipulando Elementos do DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const titulo = document.querySelector("#cadastrar-usuario");
+/*const titulo = document.querySelector("#cadastrar-usuario");
 titulo.textContent = "Add User"; //usa em query selector
 
 const btnCadastar = document.getElementById("cadastrar");
@@ -13,3 +13,17 @@ const itens = document.getElementsByClassName("item"); //devolve um html collect
 
 const tagItens = document.getElementsByTagName("li");
 console.log(tagItens);
+*/
+
+const items = document.querySelector(".items");
+
+//items.remove();//remove da página toda a lista
+//items.firstElementChild.remove();//elimina o primeiro elemento da lista
+//items.lastElementChild.remove();//elimina o ultimo elemento da lista
+
+// items.children[1].textContent = "Item 2 Novo"; //muda o testo do elemento na posição 1
+
+// items.lastElementChild.innerHTML = "<h3>Olá</h3>";
+
+const botao = document.getElementById("cadastrar");
+botao.style.background = "cyan"; //muda a cor do botão
